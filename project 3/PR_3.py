f88b58535842ab8b5c613ce897a9c350ee0959e6
iamdivyeshjadav <divyeshjadav2008@gmail.com>
2026-05-28 16:13:52 +0530

    python

--- project 3/PR_3.py
+++ project 3/PR_3.py
@@ -67,3 +67,102 @@ while True:
             break
         case _:
             print("invalid choice. please try again.")
+"""
+output:
+welcome to the student data organizer
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 1
+-------------------------------
+enter student ID: 101
+enter student name: divyesh
+enter student age: 17
+enter student grade: A
+enter student date of birth (YYYY-MM-DD): 2000-12-08
+enter student subject (comma-separated): Maths,Sci,Eng
+student added successfully.
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 2
+-------------------------------
+ID: 101, Name: divyesh, Age: 17, Grade: A, Date of Birth: 2000-12-08, Subject: Maths,Sci,Eng
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 3
+-------------------------------
+enter student ID to update: 
+student ID not found.
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 3
+-------------------------------
+enter student ID to update: 101
+enter new student name: neev
+enter new student age: 21
+enter new student grade: A
+enter new student date of birth (YYYY-MM-DD): 2005-12-01
+enter new student subject (comma-separated): Maths,Eng
+student data updated successfully.
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 4
+-------------------------------
+enter student ID to delete: 101
+student data deleted successfully.
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 5
+-------------------------------
+subjects offered:
+Math
+Science
+History
+English
+Art
+-------------------------------
+please select an option:
+press 1 for  add a student
+press 2 for  display student data
+press 3 for  update student data
+press 4 for  delete student data
+press 5 for  display subject offered
+press 6 for  exit
+enter your choice: 6
+thank you for using the student data organizer. goodbye!
+"""
